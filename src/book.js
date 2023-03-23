93c45a81a7088247b184017ef771653075f1a071
Matt Fowler <mbfowler@gmail.com>
2023-03-23 18:37:19 -0400

    wrote all book functions to pass all tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,50 @@
-function createTitle() {
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pons) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pons
+  }
+}
+
+function saveReview(review, reviews) {
+  if(reviews.includes(review)) {
+    return reviews;
+    } else {
+      reviews.push(review);
+      return reviews;
+    }
   
+}
+
+function calculatePageCount(bookTitle) {
+  var chars = bookTitle.split("");
+  return chars.length * 20;
+  
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: genre
+  }
+  return book;
+}
+
+function editBook(book) {
+  book.pageCount *= .75;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
