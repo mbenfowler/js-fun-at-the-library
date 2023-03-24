d5907ad863f5cd916295c70a1055d1ceabf857e7
Matt Fowler <mbfowler@gmail.com>
2023-03-23 23:18:32 -0400

    completes shelf functions and tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,41 @@
+function shelfBook(book, shelf) {
+  if(shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+}
+
+function unshelfBook(bookName, shelf) {
+  for(i = 0; i < shelf.length; i++){
+    if(shelf[i].title === bookName) {
+      shelf.splice(i, 1);
+    }
+  }
+  
+  return shelf;
+}
+
+function listTitles(books) {
+  titles = [];
+  for(i = 0; i < books.length; i++) {
+    titles.push(books[i].title);
+  }
+
+  return titles.join(", ");
+}
+
+function searchShelf(shelf, title) {
+  for(i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === title) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
